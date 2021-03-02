9959d9dcd133444ffa3e326d6934970a8b09a523
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2021-03-02 11:01:22 +0100

    Add project files.

---
+++ plock m1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace plock_m1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
