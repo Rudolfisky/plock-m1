e1153b8570d73237aaddf068310c135a52841e1a
Rudolfisky <33927733+Rudolfisky@users.noreply.github.com>
2021-03-05 14:57:42 +0100

    dag list

--- plock m1/Controllers/HomeController.cs
+++ plock m1/Controllers/HomeController.cs
@@ -27,6 +27,32 @@ namespace plock_m1.Controllers
         {
             return View();
         }
+        public IActionResult DagLoggen()
+        {
+            return View();
+        }
+        public IActionResult DagList()
+        {
+            List<dag> dagen = new List<dag>();
+            dagen.Add(new dag(new DateTime(2021, 1, 1), null, "20:00", "01:00", 20));
+            dagen.Add(new dag(new DateTime(1999, 7, 26), null, "20:00", "01:00", 20));
+            dagen.Add(new dag(new DateTime(2021, 1, 2), null, "20:00", "01:00", 20));
+            dagen.Add(new dag(new DateTime(2021, 1, 3), null, "20:00", "01:00", 20));
+            dagen.Add(new dag(new DateTime(2011, 1, 3), null, "20:00", "01:00", 20));
+            dagen.Add(new dag(new DateTime(2021, 1, 4), null, "20:00", "01:00", 20));
+            dagen.Add(new dag(new DateTime(2021, 10, 10), "Stones avond", "20:00", "01:00", 162));
+            //for (int i = 0; i < 50; i++)
+            //{
+            //    dagen.Add(new dag(new DateTime(2021, 10, 10), "avond", "20:00", "01:00"));
+            //}
+            List<dag> SortedDagen = dagen.OrderBy(o => o.Date).ToList();
+            SortedDagen.Reverse();
+            return View(SortedDagen);
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
